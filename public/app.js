8aa4c09515ef2764ab8bf59ac84ff3a64ae6688f
Perman <alexander.perman@finastra.com>
2021-06-30 15:37:19 +0300

    first

---
+++ public/app.js
@@ -0,0 +1,45 @@
+/* eslint-env browser */
+// main.js
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ => {
+  fetch('/customers', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vadar',
+      number: 1234,
+      address: 'The Street 23 London',
+      phone: '052-76252334'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(response => {
+      window.location.reload(true)
+    })
+})
+
+deleteButton.addEventListener('click', _ => {
+  fetch('/customers', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vadar'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(response => {
+      if (response === 'No quote to delete') {
+        messageDiv.textContent = 'No Darth Vadar quote to delete'
+      } else {
+        window.location.reload(true)
+      }
+    })
+    .catch(console.error)
+})
---
+++ routes/customers.js
@@ -0,0 +1,74 @@
+var express = require("express");
+var router = express.Router();
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://alexperman:koza2020@cluster0.yqqke.mongodb.net/thefloor?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+router.get("/", (req, res) => {
+  client.connect(err => {
+    if (err) return console.error(err)
+    const db = client.db('thefloor')
+    db.collection("customers")
+      .find()
+      .toArray()
+      .then((customers) => {
+        res.render("customers/list.ejs", { customers: customers });
+      })
+      .catch(/* ... */);
+  });
+});
+
+router.post("/customers", (req, res) => {
+  client.connect(err => {
+    if (err) return console.error(err)
+    const db = client.db('thefloor')
+    db.collection("customers")
+      .insertOne(req.body)
+      .then((result) => {
+        res.redirect("/customers");
+      })
+      .catch((error) => console.error(error));
+  });
+});
+
+router.put("/customers", (req, res) => {
+  client.connect(err => {
+    if (err) return console.error(err)
+    const db = client.db('thefloor')
+    db.collection("customers")
+    .findOneAndUpdate(
+      { name: "First Customer" },
+      {
+        $set: {
+          name: req.body.name,
+          numer: req.body.number,
+          address: req.body.address,
+          phone: req.body.phone
+        },
+      },
+      {
+        upsert: true,
+      }
+    )
+    .then((result) => res.json("Success"))
+    .catch((error) => console.error(error));
+  });
+});
+
+router.delete("/customers", (req, res) => {
+  client.connect(err => {
+    if (err) return console.error(err)
+    const db = client.db('thefloor')
+    db.collection("customers")
+    .deleteOne({ name: req.body.name })
+    .then((result) => {
+      if (result.deletedCount === 0) {
+        return res.json("No customer to delete");
+      }
+      res.json("Deleted customer");
+    })
+    .catch((error) => console.error(error));
+  });
+});
+
+module.exports = router;
